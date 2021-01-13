50d3a2de8d170d768de5eadaa9e5fbe8c14b4af9
Mounir Missaoui <mmissaoui@mbazconsulting.com>
2021-01-13 11:23:15 +0100

    full on flip

--- src/portfolio.js
+++ src/portfolio.js
@@ -304,8 +304,8 @@ const podcastSection = {
 const contactInfo = {
   title: emoji("Contact Me ☎️"),
   subtitle: "Discuss a project or just want to say hi? My Inbox is open for all.",
-  number: "+92-3243454077",
-  email_address: "saadpasta70@gmail.com"
+  number: "+216-29958376",
+  email_address: "mounir.missaoui@esprit.tn"
 };
 
 //Twitter Section
